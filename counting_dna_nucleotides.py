1fc42e9d441f5ef021859d9c54907638038205e4
Timmy Nguyen <timmynguyen@Timmys-iMac.local>
2017-08-22 10:13:21 -0700

    added credentials

--- counting_dna_nucleotides.py
+++ counting_dna_nucleotides.py
@@ -17,41 +17,45 @@ input: AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC
 
 output: 20 12 17 21 # A, C, G, T format
 """
-print(*map(input().count, "ACGT"))
-# # Create function to read nucleotides
-# def read_DNA(nucleotide):
-# 	nucleotide.upper() 						# convert string to uppercase
+
+# Create function to read nucleotides
+def read_DNA(nucleotide):
+	nucleotide.upper() 						# convert string to uppercase
 	
-# 	# Purines Count
-# 	num_Adenine = nucleotide.count('A')		# count the occurence of A
-# 	num_Guanine = nucleotide.count('G')		# count the occurence of G
+	# Purines Count
+	num_Adenine = nucleotide.count('A')		# count the occurence of A
+	num_Guanine = nucleotide.count('G')		# count the occurence of G
 
-# 	# Pyrimidines Count
-# 	num_Thymine = nucleotide.count('T')		# count the occurence of T
-# 	num_Cytosine = nucleotide.count('C')	# count the occurence of C
+	# Pyrimidines Count
+	num_Thymine = nucleotide.count('T')		# count the occurence of T
+	num_Cytosine = nucleotide.count('C')	# count the occurence of C
 
-# 	# space = " "
-# 	# result = num_Adenine + space + num_Cytosine + space + num_Guanine + space + num_Thymine
-# 	result = [num_Adenine,num_Cytosine, num_Guanine, num_Thymine]
+	# space = " "
+	# result = num_Adenine + space + num_Cytosine + space + num_Guanine + space + num_Thymine
+	result = [num_Adenine,num_Cytosine, num_Guanine, num_Thymine]
 
-# 	return result
+	return result
 
-# # Prompt for user input
-# user_prompt = input("Enter nucleotide string: ")
+# Prompt for user input
+user_prompt = input("Enter nucleotide string: ")
 
-# # Get results
-# dna_count = read_DNA(user_prompt)
+# Get results
+dna_count = read_DNA(user_prompt)
 
-# # Output to console
-# print (dna_count)	
+# Output to console
+print (dna_count)	
 
 ''' =========================================================== 
+# Other solutions
+
 Alternative #1:
+@Maurício Verardo da Costa
     
     def qt(s):
       return s.count("A"), s.count("G"), s.count("C"), s.count("T")
 
 Alternative #2:
+@arekolek
 
 	freq = {'A': 0, 'C': 0, 'G': 0, 'U': 0}
 	for i in string:
@@ -60,6 +64,7 @@ Alternative #2:
 	print freq['A'], freq['C'], freq['G'], freq['U']
 	
 Alternative #3:
+@Yuriy Khamzyaev
 	# print(*map(input().count, "ACGT"))
 
 '''
